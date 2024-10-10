3001f82de96d25b3763bd1b67ab1960bf6214e9d
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2024-10-10 14:38:17 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import {Inter} from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({subsets:['latin']})
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("relative h-full font-sans antialiased", inter.className)}
       >
-        {children}
+        <main className="relative flex flex-col min-h-screen"> 
+          <Navbar/>
+          <div className="flex-grow flex-1">
+
+          {children}
+          </div>
+        </main>
       </body>
     </html>
   );
